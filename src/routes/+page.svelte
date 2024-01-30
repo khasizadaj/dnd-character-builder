4497f10ab14b9f78d7620352f30c4e04a5f5b490
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-01-30 22:08:14 +0100

    Add barebone basic info to page (#7)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import profilePicture from "$lib/assets/profile.png";
+
+    let character = {
+        profilePicture: profilePicture,
+        name: 'John Doe',
+        class: 'Wizard',
+        level: 5,
+        race: 'Human',
+        hair_color: 'Brunette',
+        skin_color: 'Dark',
+        eye_color: 'Brown',
+        height: '220',
+        weight: '220',
+        appearance: 'Tall with brown hair and blue eyes',
+    };
+</script>
+
+<div class="section">
+    <div class="container">
+        <div class="basic">
+            <div class="character-image image">
+                <img src={character.profilePicture} alt="profile picture of character" />
+            </div>
+            
+            <h1>{character.name}</h1>
+            <article>
+                <p>Level: {character.level}</p>
+                <p>Race: {character.race}</p>
+                <p>Class: {character.class}</p>
+            </article>
+        </div>
+        
+        <div class="apperance">
+            <p>Hair: {character.hair_color}</p>
+            <p>Eye color: {character.eye_color}</p>
+            <p>Skin color: {character.skin_color}</p>
+            <p>Weight: {character.weight}</p>
+            <p>height: {character.height}</p>
+        </div>
+    </div>
+</div>
