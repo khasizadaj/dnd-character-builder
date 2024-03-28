bbc37b2c92e415df45946889ceec12dca693b4f9
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-28 03:10:26 +0100

    Add feature to read character details from file (#12)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import 'carbon-components-svelte/css/g90.css';
-	import { Tag, Grid, Row, Column, TooltipDefinition } from 'carbon-components-svelte';
-
+	import { Tag, Grid, Row, Column, Tile, FileUploader, Link } from 'carbon-components-svelte';
 	import profilePicture from '$lib/assets/dnd-character-photo.jpg';
 
 	let character = {
@@ -26,7 +25,7 @@
 			{ type: 'charisma', score: 7, proficient: false }
 		]
 	};
-
+	
 	function calculateAbilityScoreModifier(score: number) {
 		let result = Math.floor((score - 10) / 2);
 		if (result > 0) {
@@ -34,12 +33,55 @@
 		}
 		return result;
 	}
+	
+	let fileUploader;
+	const loadCharacterFile = (files) => {
+		const file = files.detail[0];
+		const reader = new FileReader();
+		reader.onload = () => {
+			try {
+				const fileContent = reader.result?.toString();
+				if (!fileContent) {
+					return;
+				}
+				const jsonData = JSON.parse(fileContent);
+				character = jsonData;
+			} catch (e) {
+				console.error('Error parsing JSON:', e);
+				return;
+			}
+		};
+
+		reader.onerror = (error) => {
+			console.error('File reading error:', error);
+			return;
+		};
+		reader.readAsText(file);
+	};
 </script>
 
-<Grid>
-	<Row padding>
-		<Column noGutter padding md={1} lg={2}></Column>
-		<Column noGutter padding md={14} lg={12}>
+<Grid narrow padding>
+	<Row>
+		<Column noGutter md={1} lg={2}></Column>
+		<Column noGutter md={14} lg={12}>
+			<Tile>
+				<FileUploader
+					bind:this={fileUploader}
+					labelTitle="Add character details"
+					buttonLabel="Add file"
+					accept={['.json	']}
+					status="complete"
+					on:change={(files) => loadCharacterFile(files)}
+				>
+					<span slot="labelDescription"
+						>Only JSON files are accepted. See sample character file
+						<Link
+							target="_blank"
+							href="https://gist.github.com/khasizadaj/66804c314e9e31b0d148b68057e4564a">here</Link
+						>.
+					</span>
+				</FileUploader>
+			</Tile>
 			<div class="character-details">
 				<div class="image" style="background-image: url({character.profilePicture});"></div>
 				<h1>{character.name}</h1>
@@ -102,7 +144,7 @@
 		align-items: center;
 		flex-direction: column;
 		gap: 1rem;
-		margin-inline: 1rem;
+		margin: 1rem;
 	}
 
 	.character-details .image {
@@ -142,7 +184,7 @@
 		font-weight: 500;
 		font-size: 1rem;
 	}
-	
+
 	.ability.proficient {
 		background: #24a148;
 	}
@@ -151,12 +193,12 @@
 		font-weight: bold;
 		padding-block: 0.5rem;
 	}
-	
+
 	.ability .type {
 		font-weight: bold;
 		padding-block: 0.5rem;
 	}
-	
+
 	.ability .modifier {
 		text-align: center;
 		width: 5rem;
