1bbbd11d8047df35380e480e01638f6619b756f3
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-29 18:50:31 +0000

    remove old code

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,6 @@
-import { readFileSync } from 'fs';
 import character from '../uploads/jkhasizada.json'
 
 export function load() {
-	let filename = 'src/uploads/jkhasizada.json';
-	const content = readFileSync(filename, 'utf8');
-	let data = JSON.parse(content);
 	return {
 		character: character
 	};
