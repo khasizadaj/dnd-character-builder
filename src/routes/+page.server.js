540181a422a31159ad3fc5a6045d03a9d4dc0c74
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-29 18:49:21 +0000

    Fix file issue

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,11 @@
 import { readFileSync } from 'fs';
+import character from '../uploads/jkhasizada.json'
 
 export function load() {
 	let filename = 'src/uploads/jkhasizada.json';
 	const content = readFileSync(filename, 'utf8');
 	let data = JSON.parse(content);
 	return {
-		character: data
+		character: character
 	};
 }
