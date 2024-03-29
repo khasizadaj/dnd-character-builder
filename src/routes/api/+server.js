b6444661eb6375d31609c6240d7df4a142f8404c
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-29 17:47:20 +0000

    Read from saved file at load time

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,26 +1,26 @@
-import { writeFile } from "fs/promises";
+import { writeFile } from 'fs/promises';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
-    try {
-        const data = await request.json();
-        const filename = `src/uploads/${data.username}.json`;
-        await writeFile(filename, JSON.stringify(data.character), 'utf8');
+	try {
+		const data = await request.json();
+		const filename = `src/uploads/${data.filename}`;
+		await writeFile(filename, JSON.stringify(data.character), 'utf8');
 
-        return json({
-            status: 200, // HTTP status code
-            body: {
-                success: true,
-                message: "Data processed successfully."
-            }
-        });
-    } catch (error) {
-        return json({
-            status: 500, // Server error status
-            body: {
-                success: false,
-                message: "Failed to process data."
-            }
-        });
-    }
+		return json({
+			status: 200,
+			body: {
+				success: true,
+				message: 'Data processed successfully.'
+			}
+		});
+	} catch (error) {
+		return json({
+			status: 500,
+			body: {
+				success: false,
+				message: 'Failed to process data.'
+			}
+		});
+	}
 }
