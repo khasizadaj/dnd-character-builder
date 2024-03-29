8f33957ab74e5f2366fabaddd37cd5062eaadd63
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-29 20:17:42 +0100

    Fix file issue (#16)
    Drop feature to write to server and fix reading problem
    
    start from the default character and add yours (deleted after refresh)

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,7 @@
-import { readFileSync } from 'fs';
+import character from '../uploads/jkhasizada.json'
 
 export function load() {
-	let filename = 'src/uploads/jkhasizada.json';
-	const content = readFileSync(filename, 'utf8');
-	let data = JSON.parse(content);
 	return {
-		character: data
+		character: character
 	};
 }
