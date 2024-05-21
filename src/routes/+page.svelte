c253295ffa63b670c36f320d0a13748f9dca8ab2
khasizadaj <javid.khasizada@gmail.com>
2024-05-21 18:13:33 +0200

    Update file uploaded copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,19 +80,21 @@
 <Tile>
 	<FileUploader
 		bind:this={fileUploader}
-		labelTitle="Add character details"
+		labelTitle="Add your character"
 		buttonLabel="Add file"
 		accept={['.json	']}
 		status="complete"
 		on:change={(files) => loadCharacterFile(files)}
 	>
-		<span slot="labelDescription">
+		<article slot="labelDescription">
 			Only JSON files are accepted. See sample character file
 			<Link
 				target="_blank"
 				href="https://gist.github.com/khasizadaj/66804c314e9e31b0d148b68057e4564a">here</Link
 			>.
-		</span>
+			<p style="font-size: inherit"><strong>[ NOTE ]</strong> Currently sample data is loaded from example character of mine.
+			</p>
+		</article>
 	</FileUploader>
 </Tile>
 <div class="character-details">
