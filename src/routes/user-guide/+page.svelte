3178d5128e5a0b33827cd27f7f489e9796a0787f
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-07-01 23:57:19 +0200

    Add weapons section to user guide (#70)
    Extra: Add library to let Prettier sort Tailwind CSS classes

--- src/routes/user-guide/+page.svelte
+++ src/routes/user-guide/+page.svelte
@@ -11,8 +11,6 @@
 	import Weapon from '$lib/components/Weapon.svelte';
 
 	import character from '$lib/assets/demo-character.json';
-	import Features from '$lib/components/Features.svelte';
-	import Health from '$lib/components/Health.svelte';
 	import { InformationSquareFilled } from 'carbon-icons-svelte';
 	import AbilityScores from '$lib/components/AbilityScores.svelte';
 	import CharacterDetails from '$lib/components/CharacterDetails.svelte';
@@ -50,9 +48,9 @@
 			get started.
 		</p>
 	</div>
-	<div class="mb-4 p-4 bg-emerald-700 w-full h-full">
+	<div class="mb-4 h-full w-full bg-emerald-700 p-4">
 		<InformationSquareFilled size={24} class="mb-2" />
-		<h4 class="mb-1 text-lg font-bold text-left">Limited To Single Character</h4>
+		<h4 class="mb-1 text-left text-lg font-bold">Limited To Single Character</h4>
 		<p>
 			At the moment, the DnD Character Builder supports only one character at a time. This ensures a
 			focused and detailed character management experience, though support for multiple characters
@@ -79,7 +77,7 @@
 			<ListItem
 				>Go to our <a
 					href="/account/signup"
-					class="text-blue-400 hover:underline decoration-blue-400 hover:underline-offset-2"
+					class="text-blue-400 decoration-blue-400 hover:underline hover:underline-offset-2"
 					>Signup</a
 				> page.</ListItem
 			>
@@ -98,7 +96,7 @@
 			<p class="mb-4">
 				On this part of the application you can find basic information about your character:
 			</p>
-			<OrderedList expressive class="ml-6">
+			<OrderedList expressive class="mb-4 ml-6">
 				<ListItem>Picture of your character</ListItem>
 				<ListItem>Name of your character</ListItem>
 				<ListItem>Level & Class name</ListItem>
@@ -106,35 +104,66 @@
 				<ListItem>Ability scores</ListItem>
 				<ListItem>Proficiency bonus & AC & Speed & Initiative</ListItem>
 			</OrderedList>
-		</div>
-		<div class="border-solid border-emerald-700 border-2">
-			<div class="mb-8 flex items-center gap-2 p-4 border-b-2 border-solid border-emerald-700">
-				<InformationSquareFilled size={24} class="text-emerald-700" />
-				<h5 class="text-lg text-emerald-700">Sample</h5>
+			<div class="border-2 border-solid border-emerald-700">
+				<div class="mb-8 flex items-center gap-2 border-b-2 border-solid border-emerald-700 p-4">
+					<InformationSquareFilled size={24} class="text-emerald-700" />
+					<h5 class="text-lg text-emerald-700">Sample</h5>
+				</div>
+				<CharacterDetails {character}></CharacterDetails>
+				<AbilityScores {character}></AbilityScores>
 			</div>
-			<CharacterDetails {character}></CharacterDetails>
-			<AbilityScores {character}></AbilityScores>
+		</div>
+		<div class="mb-4">
+			<h4 class="mb-4 text-xl">Weapons</h4>
+			<p class="mb-4">
+				On this part of the application you can find all necessary information about your weapons:
+			</p>
+			<OrderedList expressive class="mb-4 ml-6">
+				<ListItem>Name of weapon</ListItem>
+				<ListItem>Weapon description (when expanded)</ListItem>
+				<ListItem>Damage dice and types</ListItem>
+				<ListItem>Weapon modifiers</ListItem>
+				<ListItem>Button to roll for attack</ListItem>
+			</OrderedList>
+			<div class="mb-4 border-2 border-solid border-emerald-700">
+				<div class="mb-4 flex items-center gap-2 border-b-2 border-solid border-emerald-700 p-4">
+					<InformationSquareFilled size={24} class="text-emerald-700" />
+					<h5 class="text-lg text-emerald-700">Sample weapon</h5>
+				</div>
+				<div class="mx-4 mb-4">
+					<Weapon weapon={sampleWeapon} {character}></Weapon>
+				</div>
+			</div>
+			<div class="border-2 border-solid border-emerald-700">
+				<div class="mb-4 flex items-center gap-2 border-b-2 border-solid border-emerald-700 p-4">
+					<InformationSquareFilled size={24} class="text-emerald-700" />
+					<h5 class="text-lg text-emerald-700">Sample weapon - Expanded</h5>
+				</div>
+				<div class="mx-4 mb-4">
+					<Weapon weapon={sampleWeapon} {character} expanded={true}></Weapon>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="mb-4">
 		<h3 class="mb-4 text-2xl">How to create character</h3>
-		<div class="mb-4 p-4 bg-orange-600 w-full h-full flex gap-2 items-center">
+		<div class="mb-4 flex h-full w-full items-center gap-2 bg-orange-600 p-4">
 			<InformationSquareFilled size={24} class="" />
-			<h4 class="text-lg font-bold text-left">Coming soon ...</h4>
+			<h4 class="text-left text-lg font-bold">Coming soon ...</h4>
 		</div>
 	</div>
 	<div class="mb-4">
 		<h3 class="mb-4 text-2xl">How to modify character</h3>
-		<div class="mb-4 p-4 bg-orange-600 w-full h-full flex gap-2 items-center">
+		<div class="mb-4 flex h-full w-full items-center gap-2 bg-orange-600 p-4">
 			<InformationSquareFilled size={24} class="" />
-			<h4 class="text-lg font-bold text-left">Coming soon ...</h4>
+			<h4 class="text-left text-lg font-bold">Coming soon ...</h4>
 		</div>
 	</div>
 	<div class="mb-4">
 		<h3 class="mb-4 text-2xl">In-game tools</h3>
-		<div class="mb-4 p-4 bg-orange-600 w-full h-full flex gap-2 items-center">
+		<div class="mb-4 flex h-full w-full items-center gap-2 bg-orange-600 p-4">
 			<InformationSquareFilled size={24} class="" />
-			<h4 class="text-lg font-bold text-left">Coming soon ...</h4>
+			<h4 class="text-left text-lg font-bold">Coming soon ...</h4>
 		</div>
 	</div>
 </section>
